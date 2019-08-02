73d4fdf7dee1660a054d2930e3e9fce9f14bdcb3
krazykoder <noddy4nods@gmail.com>
2019-08-01 22:31:00 -0700

    KNN edit

--- KNNClusterBook.py
+++ KNNClusterBook.py
@@ -1,4 +1,7 @@
-# Unsupoervised 
+# Unsupervised Learning with KMeans Clusters
+
+# https://towardsdatascience.com/clustering-based-unsupervised-learning-8d705298ae51
+# https://medium.com/datadriveninvestor/unsupervised-learning-with-python-k-means-and-hierarchical-clustering-f36ceeec919c
 
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -10,9 +13,9 @@ import numpy as np
 # XA = np.asarray(XD)
 # nsamples, nx, ny, nz = XA.shape  # know the shape before you flatten
 # X = XA.reshape ((nsamples, nx*ny*nz)) # gives a 2 D matice (sample, value) which can be fed to KMeans 
-X = XD
+# X = XD
 
-#----------------- Determint cluster size
+#---------------------------- Determine cluster size----------------------------------
 
 # find the appropriate cluster number
 plt.figure(figsize=(10, 8))
@@ -25,12 +28,12 @@ X = iris.data
 # Calculate clusters using Elbow criteria 
 
 wcss = []
-for i in range(1, 101):
+for i in range(1, 51):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
-    kmeans.fit(XD)
+    kmeans.fit(X)
     wcss.append(kmeans.inertia_)
 
-plt.plot(range(1, 101), wcss)
+plt.plot(range(1, 51), wcss)
 # plt.plot(range(1, 11), elbowIndex)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
@@ -41,14 +44,17 @@ plt.show()
 # https://github.com/arvkevi/kneed#find-knee
 
 from kneed import KneeLocator
-elbow = KneeLocator( list(range(1,101)), wcss, S=1.0, curve='convex', direction='decreasing')
+elbow = KneeLocator( list(range(1,51)), wcss, S=1.0, curve='convex', direction='decreasing')
 print ('Detected Elbow cluster value :', elbow.knee)
 
-def getElbowPointIndex(wcss):
 
-    return wcss
 
-#------------------ K-Means (Simplest model)-------------
+#------------------------------------ K-Means (Simplest model)------------------------------------
+# Additional READs
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.08-random-forests.html
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html
+# https://www.dummies.com/programming/big-data/data-science/how-to-visualize-the-clusters-in-a-k-means-unsupervised-learning-model/
+
 
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -56,21 +62,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 
-# Additional READs
-# https://jakevdp.github.io/PythonDataScienceHandbook/05.08-random-forests.html
-# https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html
-# https://www.dummies.com/programming/big-data/data-science/how-to-visualize-the-clusters-in-a-k-means-unsupervised-learning-model/
-
-
 # %matplotlib inline
-# https://towardsdatascience.com/clustering-based-unsupervised-learning-8d705298ae51
 
 from sklearn import datasets
 #Iris Dataset
 iris = datasets.load_iris()
 X = iris.data
 #KMeans
-km = KMeans(n_clusters=10)
+km = KMeans(n_clusters=3)
+# km = KMeans(n_clusters=10)  # for RBG dataset K = 33 
 km.fit(X)
 km.predict(X)
 labels = km.labels_
@@ -80,7 +80,7 @@ labels = km.labels_
 # mydataRGB['cluster'] = pd.DataFrame(labels)
 # mydataRGB[['file', 'cluster']].head
 
-#Plotting
+# Plotting
 fig = plt.figure(1, figsize=(7,7))
 ax = Axes3D(fig, rect=[0, 0, 0.95, 1], elev=48, azim=134)
 ax.scatter(X[:, 3], X[:, 0], X[:, 2],
@@ -105,7 +105,7 @@ plt.scatter(centers[:, 0], centers[:, 1], c='gray', s=200, alpha=0.5);
 
 
 
-# --------------------Hierarchial Clustering -----------------------
+# ----------------------------------Hierarchial Clustering ----------------------------------
 
 # Hierarchical clustering for the same dataset
 # creating a dataset for hierarchical clustering
@@ -117,7 +117,7 @@ import numpy as np
 # some setting for this notebook to actually show the graphs inline
 # you probably won't need this
 # %matplotlib inline
-np.set_printoptions(precision=5, suppress=True)  # suppress scientific float notation
+np.set_printoptions(precision=25, suppress=True)  # suppress scientific float notation
 #creating the linkage matrix
 H_cluster = linkage(dataset2_standardized,'ward')
 plt.title('Hierarchical Clustering Dendrogram (truncated)')
@@ -135,8 +135,7 @@ plt.show()
 
 
 
-
-# ----------------- GMM models ------------------
+# ------------------------------- GMM Clustering models ------------------------------------
 
 from sklearn.mixture import GaussianMixture
 import matplotlib.pyplot as plt
@@ -182,17 +181,30 @@ plt.title("Gaussian Mixture Model", fontsize=14)
 from sklearn.neighbors import KDTree
 
 tree = KDTree(X)  # metric = 
+# metric distance https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.DistanceMetric.html
+# KDTree.valid_metrics gives list of valid metrics 
+# Valid values for metric are: 
+# from scikit-learn:
+# ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']
+# from scipy.spatial.distance:
+# ['braycurtis', 'canberra', 'chebyshev', 'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule']
+
+
+# Search in the Kd Tree
 
 # where ft = raw feature 
 nx, ny, nz = ft.shape  # know the shape before you flatten
 F = ft.reshape ((nx*ny*nz)) # gives a 2 D matice (sample, value) which can be fed to KMeans 
 
-
 dist, ind = tree.query(F, k=9)
 
 
-# ----------------------- KD Tree---------------------
 
+
+
+
+# ----------------------- KD Tree  RGB ---------------------
+# Avg. Time per search: 0.023 s
 
 from sklearn.neighbors import KDTree
 import time 
@@ -204,15 +216,8 @@ XA = np.asarray(XD)
 nsamples, nx, ny, nz = XA.shape  # know the shape before you flatten
 X = XA.reshape ((nsamples, nx*ny*nz)) # gives a 2 D matice (sample, value) which can be fed to KMeans 
 
-
 RGBtree = KDTree(X , metric='manhattan')
-# metric distance https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.DistanceMetric.html
-# KDTree.valid_metrics gives list of valid metrics 
-# Valid values for metric are: 
-# from scikit-learn:
-# ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']
-# from scipy.spatial.distance:
-# ['braycurtis', 'canberra', 'chebyshev', 'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule']
+
 
 # random sample 
 q_path = random.sample(imagepaths, 1)[0]
@@ -226,19 +231,37 @@ ft = ImageSearch_Algo_RGB.RGB_FEATURE (q_path)
 nx, ny, nz = ft.shape  # know the shape before you flatten
 F = ft.reshape ((1, nx*ny*nz)) # gives a 2 D matice (sample, value) which can be fed to KMeans 
 
-ld = RGBtree.query(F, k=50)
-print (ind)
+dist, ind = RGBtree.query(F, k=50)
+t = time.time() - start 
 
+print (ind)
 # get the index of searchimage 
 print (mydataRGB.index[mydataRGB['file'] == q_path])
 print (q_path)
-t = time.time() - start 
+
 print ( "RGB KDTree Search took ", t, ' secs')
 # print the list of files from ind
-mydataRGB.iloc[ ind[0].tolist()]['file']
+# mydataRGB.iloc[ ind[0].tolist()]['file']
+
+# Zip results into a list of tuples (score , file) & calculate score 
+flist = list (mydataHSV.iloc[ ind[0].tolist()]['file'])
+slist = list (dist[0])
+result = tuple(zip( slist, flist)) 
+a , q = accuracy.accuracy_matches(q_path, result, 20)
+print ('Accuracy =',  a, '%', '| Quality:', q )
 
 
-# -------------------- END KD Tree---------------------
+
+
+
+
+
+
+
+
+
+# --------------------  KD Tree  HSV ---------------------
+# Avg. Time per search: 0.033 s
 
 
 from sklearn.neighbors import KDTree
@@ -249,10 +272,11 @@ YA = np.asarray(YD)
 # nsamples, nx, ny, nz = XA.shape  # know the shape before you flatten
 # X = XA.reshape ((nsamples, nx*ny*nz)) # gives a 2 D matice (sample, value) which can be fed to KMeans 
 
-HSVtree = KDTree(YA , metric='manhattan')
+HSVtree = KDTree(YA ) # , metric='euclidean')
 
 #  Example with HSV metrices 
 import ImageSearch_Algo_HSV 
+import Accuracy as accuracy
 
 # random sample 
 q_path = random.sample(imagepaths, 1)[0]
@@ -267,14 +291,19 @@ fh = np.asarray(fh)
 nz = fh.shape  # know the shape before you flatten
 F = fh.reshape (1, -1) # gives a 2 D matice (sample, value) which can be fed to KMeans 
 
-dist, ind = HSVtree.query(F, k=50)
+dist, ind = HSVtree.query(F, k=100)
+t = time.time() - start 
+
 print (ind)
 
 # get the index of searchimage 
 print (mydataHSV.index[mydataHSV['file'] == q_path])
 print (q_path)
-t = time.time() - start 
 print ( "Search took ", t, ' secs')
 
-# print the list of files from ind
-mydataHSV.iloc[ ind[0].tolist()]['file']
+# Zip results into a list of tuples (score , file) & calculate score 
+flist = list (mydataHSV.iloc[ ind[0].tolist()]['file'])
+slist = list (dist[0])
+result = tuple(zip( slist, flist)) # create a list of tuples from 2 lists 
+a , q = accuracy.accuracy_matches(q_path, result, 100)
+print ('Accuracy =',  a, '%', '| Quality:', q )
