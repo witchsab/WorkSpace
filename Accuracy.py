49fe30a561ddc02339fbdb2273fbffe042668fa2
witchsab <sabrinasult@gmail.com>
2019-08-10 20:48:13 -0700

    algo merge updated sab

--- Accuracy.py
+++ Accuracy.py
@@ -99,3 +99,94 @@ def metricCalc(indexlist) :
     return d
 
 # accuracy_matches ('./imagesbooks/ukbench07994.jpg', imagematches, 50 )
+
+
+
+
+
+def accuracy_toplist(q_path, imagelist, window):
+
+    # q_path = './imagesbooks/ukbench07994.jpg'
+    # print (q_path)
+    # imagematches = [(1.0, './imagesbooks/ukbench07994.jpg'), (0.9841861435227051, './imagesbooks/ukbench07997.jpg'), (0.9727056941785788, './imagesbooks/ukbench08000.jpg'), (0.9512209503972383, './imagesbooks/ukbench08001.jpg'), (0.9472347018503713, './imagesbooks/ukbench08048.jpg'), (0.9308509982630043, './imagesbooks/ukbench08002.jpg'), (0.9301495848422062, './imagesbooks/ukbench08062.jpg'), (0.9004718978487414, './imagesbooks/ukbench03125.jpg'), (0.8799294992301836, './imagesbooks/ukbench08051.jpg'), (0.8728566632483997, './imagesbooks/ukbench03124.jpg'), (0.8709723434067984, './imagesbooks/ukbench07993.jpg'), (0.8661491759075842, './imagesbooks/ukbench03127.jpg'), (0.8568008483230735, './imagesbooks/ukbench07996.jpg'), (0.8557715568599505, './imagesbooks/ukbench03057.jpg'), (0.8525254961024937, './imagesbooks/ukbench07992.jpg'), (0.8517494888726892, './imagesbooks/ukbench03126.jpg'), (0.8275000684980399, './imagesbooks/ukbench08060.jpg'), (0.8268087203729461, './imagesbooks/ukbench08059.jpg')]
+
+
+    acc = 0
+
+    queryfilename = os.path.basename(q_path)
+    # print (queryfilename)
+
+    filenumber = int(queryfilename.split('.')[0].split('ukbench')[1])
+    fileext = queryfilename.split('.')[1]
+
+    start = filenumber - filenumber%4
+
+    #init a list of match file names
+
+    familyfilename = []
+
+
+    for i in range(start, start+4):
+        familyfilename.append('ukbench'+ str(i).zfill(5)+ '.' + fileext)
+
+    # print(familyfilename)
+
+    try: 
+        familyfilename.remove(queryfilename)
+    except: 
+        pass
+
+
+    #init search list for accuracy score calculation
+
+    searchlist = []
+
+    for item in imagelist[:window]:
+        
+        searchlist.append(os.path.basename(item))
+
+    # print (searchlist)
+
+    try : 
+        searchlist.remove(queryfilename)
+    except: 
+        pass
+
+    overlap = list(set(familyfilename).intersection(searchlist))
+    # print ('overlap', overlap)
+
+    acc = (len(overlap)/len(familyfilename)) *100
+
+    # Accurracy metric calculation 
+
+    searchlist = []
+    for item in imagelist:
+        searchlist.append(os.path.basename(item))
+
+    # print (searchlist)
+    try : 
+        searchlist.remove(queryfilename)
+    except: 
+        pass
+
+    overlap = list(set(familyfilename).intersection(searchlist))
+    # print ('overlap', overlap)
+
+    indexlist = []
+    for common in overlap: 
+        try : 
+            indexlist.append(searchlist.index(common) + 1)
+        except : 
+            pass
+    indexlist.append(0)
+    sorted_position = sorted(indexlist)
+    
+    # print (sorted_position)
+
+    metricValue = metricCalc(indexlist)
+    # metricValue = metricCalc(list(range(0, len(familyfilename)+1))) /metricCalc(indexlist)
+    # print (metricValue)
+    # print (queryfilename)
+    # print (searchlist)
+
+    return (acc, metricValue, sorted_position, len(sorted_position) )
--- AlgoMerge.py
+++ AlgoMerge.py
@@ -24,6 +24,10 @@ import ImageSearch_Algo_SIFT
 import Accuracy as accuracy  
 from kneed import KneeLocator
 
+# to reload module: uncomment use the following
+%load_ext autoreload
+%autoreload 2
+
 IMGDIR = "./imagesbooks/"
 imagepaths = list(paths.list_images(IMGDIR))
 # print (imagepathss)
@@ -42,14 +46,15 @@ mydatasift = ImageSearch_Algo_SIFT.SIFT_LOAD_FEATURES('data/SIFT_features_519set
 mytreeHSV = ImageSearch_Algo_HSV.HSV_Create_Tree (mydataHSV, savefile='HSV_Tree')
 mytreeRGB = ImageSearch_Algo_RGB.RGB_Create_Tree (mydataRGB, savefile='RGB_Tree')
 
-q_paths = random.sample(imagepaths, 5)  # random sample 100 items in list
+# q_paths = random.sample(imagepaths, 5)  # random sample 100 items in list
 
-# q_paths = imagepaths  
+q_paths = imagepaths  
 
-# q_paths = ['./imagesbooks/ukbench05960.jpg','./imagesbooks/ukbench00459.jpg', './imagesbooks/ukbench06010.jpg', './imagesbooks/ukbench06104.jpg', './imagesbooks/ukbench00458.jpg']
+# q_paths = ['./imagesbooks/ukbench05960.jpg']  #,'./imagesbooks/ukbench00459.jpg', './imagesbooks/ukbench06010.jpg', './imagesbooks/ukbench06104.jpg', './imagesbooks/ukbench00458.jpg']
 # q_paths = ['./imagesbooks/ukbench05960.jpg', './imagesbooks/ukbench00459.jpg', './imagesbooks/ukbench06010.jpg', './imagesbooks/ukbench06104.jpg', './imagesbooks/ukbench00458.jpg', './imagesbooks/ukbench00248.jpg', './imagesbooks/ukbench06408.jpg', './imagesbooks/ukbench00303.jpg', './imagesbooks/ukbench03124.jpg', './imagesbooks/ukbench05776.jpg', './imagesbooks/ukbench06113.jpg', './imagesbooks/ukbench05964.jpg', './imagesbooks/ukbench10164.jpg', './imagesbooks/ukbench02750.jpg', './imagesbooks/ukbench05951.jpg', './imagesbooks/ukbench05983.jpg', './imagesbooks/ukbench03867.jpg', './imagesbooks/ukbench05883.jpg', './imagesbooks/ukbench06049.jpg', './imagesbooks/ukbench06017.jpg', './imagesbooks/ukbench06150.jpg', './imagesbooks/ukbench06151.jpg', './imagesbooks/ukbench02749.jpg', './imagesbooks/ukbench02721.jpg', './imagesbooks/ukbench05879.jpg', './imagesbooks/ukbench06148.jpg', './imagesbooks/ukbench05880.jpg', './imagesbooks/ukbench05929.jpg', './imagesbooks/ukbench06048.jpg', './imagesbooks/ukbench08544.jpg', './imagesbooks/ukbench03058.jpg', './imagesbooks/ukbench10154.jpg', './imagesbooks/ukbench00000.jpg', './imagesbooks/ukbench05972.jpg', './imagesbooks/ukbench05872.jpg', './imagesbooks/ukbench08542.jpg', './imagesbooks/ukbench06004.jpg', './imagesbooks/ukbench05993.jpg', './imagesbooks/ukbench05988.jpg', './imagesbooks/ukbench00483.jpg', './imagesbooks/ukbench08546.jpg', './imagesbooks/ukbench06539.jpg', './imagesbooks/ukbench02748.jpg', './imagesbooks/ukbench05980.jpg', './imagesbooks/ukbench08001.jpg', './imagesbooks/ukbench03890.jpg', './imagesbooks/ukbench03059.jpg', './imagesbooks/ukbench10081.jpg', './imagesbooks/ukbench06519.jpg', './imagesbooks/ukbench05787.jpg']
 
-toplist = set()
+# q_paths = ["./imagesbooks/ukbench00196.jpg",  "./imagesbooks/ukbench00199.jpg",  "./imagesbooks/ukbench00296.jpg",  "./imagesbooks/ukbench00298.jpg",  "./imagesbooks/ukbench00299.jpg",  "./imagesbooks/ukbench00300.jpg",  "./imagesbooks/ukbench00302.jpg",  "./imagesbooks/ukbench00303.jpg",  "./imagesbooks/ukbench02730.jpg",  "./imagesbooks/ukbench02740.jpg",  "./imagesbooks/ukbench02743.jpg",  "./imagesbooks/ukbench05608.jpg",  "./imagesbooks/ukbench05932.jpg",  "./imagesbooks/ukbench05933.jpg",  "./imagesbooks/ukbench05934.jpg",  "./imagesbooks/ukbench05935.jpg",  "./imagesbooks/ukbench05952.jpg",  "./imagesbooks/ukbench05953.jpg",  "./imagesbooks/ukbench05954.jpg",  "./imagesbooks/ukbench05955.jpg",  "./imagesbooks/ukbench05956.jpg",  "./imagesbooks/ukbench05957.jpg",  "./imagesbooks/ukbench05958.jpg",  "./imagesbooks/ukbench05959.jpg",  "./imagesbooks/ukbench06148.jpg",  "./imagesbooks/ukbench06149.jpg",  "./imagesbooks/ukbench06150.jpg",  "./imagesbooks/ukbench06151.jpg",  "./imagesbooks/ukbench06558.jpg",  "./imagesbooks/ukbench06559.jpg",  "./imagesbooks/ukbench07285.jpg",  "./imagesbooks/ukbench07588.jpg",  "./imagesbooks/ukbench07589.jpg",  "./imagesbooks/ukbench07590.jpg",  "./imagesbooks/ukbench08540.jpg",  "./imagesbooks/ukbench08542.jpg",  "./imagesbooks/ukbench08592.jpg", "./imagesbooks/ukbench08594.jpg","./imagesbooks/ukbench08595.jpg","./imagesbooks/ukbench08609.jpg","./imagesbooks/ukbench09364.jpg","./imagesbooks/ukbench09365.jpg","./imagesbooks/ukbench09366.jpg","./imagesbooks/ukbench10061.jpg","./imagesbooks/ukbench10065.jpg","./imagesbooks/ukbench10066.jpg","./imagesbooks/ukbench10085.jpg","./imagesbooks/ukbench10087.jpg","./imagesbooks/ukbench10108.jpg","./imagesbooks/ukbench10109.jpg","./imagesbooks/ukbench10110.jpg","./imagesbooks/ukbench10112.jpg","./imagesbooks/ukbench10113.jpg","./imagesbooks/ukbench10114.jpg","./imagesbooks/ukbench10116.jpg","./imagesbooks/ukbench10118.jpg","./imagesbooks/ukbench10119.jpg","./imagesbooks/ukbench10124.jpg","./imagesbooks/ukbench10125.jpg","./imagesbooks/ukbench10126.jpg","./imagesbooks/ukbench10128.jpg","./imagesbooks/ukbench10129.jpg","./imagesbooks/ukbench10130.jpg","./imagesbooks/ukbench10131.jpg","./imagesbooks/ukbench10152.jpg","./imagesbooks/ukbench10153.jpg","./imagesbooks/ukbench10154.jpg","./imagesbooks/ukbench10164.jpg","./imagesbooks/ukbench10165.jpg","./imagesbooks/ukbench10166.jpg","./imagesbooks/ukbench10167.jpg"]
+
 
 accStatsmerge = pd.DataFrame(columns=['file'])
 matcheshsv = []
@@ -57,32 +62,36 @@ matchesrgb = []
 
 for q_path in q_paths: 
 
+    toplist = []
+
     start = time.time()
 
     row_dict = {'file':q_path }   
     imagematcheshsv , searchtimehsv = ImageSearch_Algo_HSV.HSV_SEARCH_TREE (mytreeHSV, mydataHSV, q_path, 100)
-    x = set(shortlist (imagematcheshsv))
-    toplist.add(x)
 
-
-    imagematchesrgb , searchtimergb = ImageSearch_Algo_RGB.RGB_SEARCH_TREE (mytreeRGB, mydataRGB, q_path, 100)
-    y= set(shortlist (imagematchesrgb))
-    toplist.add(y)
-
-
-    matcheshsv.append((q_path, imagematcheshsv))
-    matchesrgb.append((q_path, imagematchesrgb))
+    x = shortlist (imagematcheshsv)
+    toplist = toplist + x
+    # print (x)
     a, d, i_hsv, cnt = accuracy.accuracy_matches(q_path, imagematcheshsv, 20)
     row_dict['hsv_acc'] = a
     row_dict['hsv_matchindex'] = i_hsv
     row_dict['hsv_Count'] = cnt
     row_dict['hsv_time'] = searchtimehsv
+    print ('HSV Accuracy =',  a, '%', '| Quality:', d )
+    print ('Count', cnt, ' | position', i_hsv)
+  
 
+    imagematchesrgb , searchtimergb = ImageSearch_Algo_RGB.RGB_SEARCH_TREE (mytreeRGB, mydataRGB, q_path, 100)
+    y= shortlist (imagematchesrgb)
+    toplist = toplist + y
+    # print (y)
     a, d, i_rgb, cnt = accuracy.accuracy_matches(q_path, imagematchesrgb, 20)
     row_dict['rgb_acc'] = a
     row_dict['rgb_matchindex'] = i_rgb
     row_dict['rgb_Count'] = cnt
     row_dict['rgb_time'] = searchtimergb
+    print ('RGB Accuracy =',  a, '%', '| Quality:', d )
+    print ('Count', cnt, ' | position', i_rgb)
 
     
     filteredsift = merge_result(imagematcheshsv, imagematchesrgb, mydatasift)
@@ -90,7 +99,7 @@ for q_path in q_paths:
     imagepredictions , searchtimesift = ImageSearch_Algo_SIFT.SIFT_SEARCH(filteredsift, q_path, 100, 0.6, 50)
     a ,d, i_sift, cnt = accuracy.accuracy_matches(q_path, imagepredictions, 20 )
     print (q_paths.index(q_path), q_path)
-    print ('Accuracy =',  a, '%', '| Quality:', d )
+    print ('SIFT Merge Accuracy =',  a, '%', '| Quality:', d )
     print ('Count', cnt, ' | position', i_sift)
 
     row_dict['siftmerge_acc'] = a
@@ -103,6 +112,34 @@ for q_path in q_paths:
     t = time.time() - start
     row_dict['totaltime'] = t
     print ('total processing time = ', t, 'seconds')
+    # print( toplist)
+
+
+    for myitem in imagepredictions[:20]:
+        x, y = myitem
+        toplist.append(y)
+
+    
+    # print( toplist)
+
+    # print(f7(toplist))
+    toplist = f7(toplist)
+
+    a ,d, i_toplist, cnt = accuracy.accuracy_toplist(q_path, toplist, 20 )
+    print (q_paths.index(q_path), q_path)
+    print ('Final Accuracy =',  a, '%', '| Quality:', d )
+    print ('Final Count', cnt, ' | position', i_toplist)
+
+    row_dict['toplistmerge_acc'] = a
+    row_dict['toplistmerge_matchindex'] = i_toplist
+    row_dict['toplistmerge_Count'] = cnt
+    row_dict['toplistmerge_quality'] = d
+    # row_dict['toplistmerge_time'] = searchtimetoplist
+    # print ("toplistmerge processing time", searchtimetoplist, 'sec')
+
+    print ("-------------------------------------------")
+
+    
     
     
     #Sift code only
@@ -126,7 +163,7 @@ for q_path in q_paths:
 # print ('HSV Tree Mean Search time', accStatsmerge['hsv'+'_time'].mean())
 # print ("Mean Acc = ", accStatsmerge['hsv' + '_predict20'].mean())
 
-accStatsmerge.to_csv('accStatsmergetestall.csv')
+accStatsmerge.to_csv('accStatsmergetestmerge519.csv')
 
 
 
@@ -159,7 +196,8 @@ def merge_result(imagematches1, imagematches2, mydataframe):
 
 
 
-    #merge with other algos after thresholding
+
+# merge with other algos after thresholding
 
 def shortlist (imagematches):
     score = []
@@ -175,9 +213,20 @@ def shortlist (imagematches):
     for i in i_rgb: 
         successScore.append(score[i])
     
-    elbow = KneeLocator( list(range(0,len(score))), score, S=2.0, curve='convex', direction='increasing')
-    print ('Detected Elbow cluster value :', elbow.knee)
+    # setting default value  
+    myknee = 0
+    try: 
+        elbow = KneeLocator( list(range(0,len(score))), score, S=2.0, curve='convex', direction='increasing')
+        myknee = elbow.knee
+        print ('Detected Elbow cluster value :', myknee)
+    except: 
+        pass
 
-    qualifiedItems = min (elbow.knee , 6)
+    qualifiedItems = min (myknee , 6)
 
-    return filelist[:6]
+    return filelist[:qualifiedItems]
+
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
